e5189eb4b8627ff0983b7b507338359056848859
JuanMorello506 <morellojuan992@gmail.com>
2025-10-17 23:24:46 -0300

    Login sign in basic logic

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class Home {
-  isLoggedIn: boolean = inject(AuthService).isLoggedIn();
+  authService = inject(AuthService);
 }
--- src/app/pages/login-sign-in/login-sign-in.ts
+++ src/app/pages/login-sign-in/login-sign-in.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
-import { AppRoutingModule } from "../../app.routes";
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login-sign-in',
   standalone: true,
-  imports: [AppRoutingModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login-sign-in.html',
   styleUrls: ['./login-sign-in.css']
 })
 export class LoginSignIn {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
-  isLoggedIn: boolean = false;
+  submit(event: Event) {
+    event.preventDefault();
 
-  toggleLogin() {
-    this.isLoggedIn = !this.isLoggedIn;
+    this.authService.login();
+
+    this.router.navigate(['dashboard']);
   }
-
 }
